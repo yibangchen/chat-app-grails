9fdd2c68d97806ed5e1b60e554f0740fbe909703
yibang <yibang.chen@saviynt.com>
2019-09-20 15:05:31 -0700

    installed spring-security-core and s2-quickstart

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -119,3 +119,22 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'chatapp.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'chatapp.UserRole'
+grails.plugin.springsecurity.authority.className = 'chatapp.Role'
+grails.plugin.springsecurity.requestMap.className = 'chatapp.Requestmap'
+grails.plugin.springsecurity.securityConfigType = 'Requestmap'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                ['permitAll'],
+	'/index':           ['permitAll'],
+	'/index.gsp':       ['permitAll'],
+	'/assets/**':       ['permitAll'],
+	'/**/js/**':        ['permitAll'],
+	'/**/css/**':       ['permitAll'],
+	'/**/images/**':    ['permitAll'],
+	'/**/favicon.ico':  ['permitAll']
+]
+
